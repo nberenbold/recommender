c1bb190358cd2fce3f74f3f40b6acef30105c7fe
Nils Berenbold <nils.berenbold@gmail.com>
2015-10-10 19:11:04 +0200

    minor changes

--- lib/recommender/controller_helper.rb
+++ lib/recommender/controller_helper.rb
@@ -2,8 +2,24 @@ module Recommender
   module ControllerHelper
     extend ActiveSupport::Concern
 
-    def is_known_crawler?
-      bot_detector.is_known_crawler?
+    def recommendations_for item, options = {}
+      limit = options.delete(:limit) || 4
+      ids = []
+      begin
+        timeout(2) do
+          ids = JSON.parse(open("http://v1.recommendation.niv-ventures.com/predict/#{item.class.to_s.downcase}/#{record.id}.json?user_id=#{user_id}&access_token=#{access_token}&limit=#{limit}").read)
+        end
+      rescue
+      end
+      ids
+    end
+
+    def user_id
+      "2"
+    end
+
+    def access_token
+      "1234"
     end
   end
 end
