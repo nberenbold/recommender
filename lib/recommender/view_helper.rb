875fe3bc929b114a669bc5ebcc7cea11f198822a
Nils Berenbold <nils.berenbold@gmail.com>
2015-10-11 16:43:25 +0200

    js optimizations

--- lib/recommender/view_helper.rb
+++ lib/recommender/view_helper.rb
@@ -28,14 +28,20 @@ module Recommender
 
       content_tag :script do
         "(function() {
-          var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-          po.src = '//v1.recommendation.niv-ventures.com/script.js';
-          po.onload = function() {
+          if (window.track_recommendation) {
             window.track_recommendation(#{Recommender.config.user_id}, [
               { event: \"#{event}\", object: \"#{object_id}\", type: \"#{object_type}\", user: \"#{user_id}\" }
             ]);
+          } else {
+            var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+            po.src = '//v1.recommendation.niv-ventures.com/script.js';
+            po.onload = function() {
+              window.track_recommendation(#{Recommender.config.user_id}, [
+                { event: \"#{event}\", object: \"#{object_id}\", type: \"#{object_type}\", user: \"#{user_id}\" }
+              ]);
+            }
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
           }
-          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
         })();".html_safe
       end
     end
