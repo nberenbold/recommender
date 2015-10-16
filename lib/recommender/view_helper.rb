4234aed69b79b8cdb7aa61db51a2b9bc89a71cc6
Nils Berenbold <nils.berenbold@gmail.com>
2015-10-16 03:24:29 +0200

    increased version of script

--- lib/recommender/view_helper.rb
+++ lib/recommender/view_helper.rb
@@ -11,7 +11,7 @@ module Recommender
       content_tag :script do
         "(function() {
           var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-          po.src = '//static.#{Recommender.config.domain}/script/v1.js';
+          po.src = '//static.#{Recommender.config.domain}/script/v2.js';
           var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
         })();".html_safe
       end
